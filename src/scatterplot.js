28d593ca64b271b2ef728834dfabc0bccbb707ef
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-01-18 15:28:09 +0100

    create Hint-content with styling

--- src/scatterplot.js
+++ src/scatterplot.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 import {
-  Hint,
-  HorizontalGridLines,
-  MarkSeries,
-  VerticalGridLines,
-  XAxis,
-  XYPlot,
-  YAxis
-} from 'react-vis';
-
+  Hint, HorizontalGridLines, MarkSeries,
+  VerticalGridLines, XAxis, XYPlot, YAxis } from 'react-vis';
+import HintContent from './hint-content.js';
 
 class scatterplot extends Component {
+  constructor(props){
+    super(props);
+
+    this.state= {
+      value: null
+    }
+
+    this._rememberValue = this._rememberValue.bind(this)
+    this._forgetValue = this._forgetValue.bind(this)
+  }
+
+    _rememberValue(value){
+      this.setState({value})
+    }
+    _forgetValue(){
+      this.setState({
+        value: null
+      })
+    }
+
+
   render() {
     return (
       <div>
